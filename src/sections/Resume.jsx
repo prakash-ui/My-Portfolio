52632aa9da412ff20d88802d73a0e52ad25254bf
Prakash <prakashtimilsina67@gmail.com>
2025-05-31 18:10:10 +0530

    added Resume

--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -4,7 +4,7 @@ export default function Resume() {
       <h2 className="text-3xl font-bold mb-6 text-center">Resume</h2>
       <div className="text-center">
         <a
-          href="/public/Resume.pdf"
+          href="https://drive.google.com/file/d/1JPecIwWUrX80JLFUhkD8SPL6Paspy1Mi/view?usp=sharing"
           download="Prakash_Timilsina_Resume.pdf" 
           className="px-6 py-2 bg-black text-white dark:bg-white dark:text-black rounded-lg hover:scale-105 transition"
         >
